48ac3ce8b5f5156d5c0295f5b2fbc6d8b3246656
Alguiris Ureña <alguirisurena12@gmail.com>
2023-07-04 15:39:57 -0400

    si

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,38 +1,38 @@
 class Controller {
-    constructor(model, view) {
-      this.model = model;
-      this.view = view;
-  
-      // Display initial todos
-      this.onTodoListChanged(this.model.todos);
-  
-      // Explicit this binding
-      this.model.bindTodoListChanged(this.onTodoListChanged);
-      this.view.bindAddTodo(this.handleAddTodo);
-      this.view.bindEditTodo(this.handleEditTodo);
-      this.view.bindDeleteTodo(this.handleDeleteTodo);
-      this.view.bindToggleTodo(this.handleToggleTodo);
-    }
-  
-    onTodoListChanged = todos => {
-      this.view.displayTodos(todos);
-    };
-  
-    handleAddTodo = todoText => {
-      this.model.addTodo(todoText);
-    };
-  
-    handleEditTodo = (id, todoText) => {
-      this.model.editTodo(id, todoText);
-    };
-  
-    handleDeleteTodo = id => {
-      this.model.deleteTodo(id);
-    };
-  
-    handleToggleTodo = id => {
-      this.model.toggleTodo(id);
-    };
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+
+    // Display initial todos
+    this.onTodoListChanged(this.model.todos);
+
+    // Explicit this binding
+    this.model.bindTodoListChanged(this.onTodoListChanged);
+    this.view.bindAddTodo(this.handleAddTodo);
+    this.view.bindEditTodo(this.handleEditTodo);
+    this.view.bindDeleteTodo(this.handleDeleteTodo);
+    this.view.bindToggleTodo(this.handleToggleTodo);
   }
-  
-  export { Controller };
+
+  onTodoListChanged = todos => {
+    this.view.displayTodos(todos);
+  };
+
+  handleAddTodo = todoText => {
+    this.model.addTodo(todoText);
+  };
+
+  handleEditTodo = (id, todoText) => {
+    this.model.editTodo(id, todoText);
+  };
+
+  handleDeleteTodo = id => {
+    this.model.deleteTodo(id);
+  };
+
+  handleToggleTodo = id => {
+    this.model.toggleTodo(id);
+  };
+}
+
+export { Controller };
--- src/scripts/model.js
+++ src/scripts/model.js
@@ -1,57 +1,57 @@
 class Model {
-    constructor() {
-      // The state of the model, an array of todo objects
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-    }
-  
-    bindTodoListChanged(callback) {
-      this.onTodoListChanged = callback;
-    }
-  
-    _commit(todos) {
-      this.onTodoListChanged(todos);
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
-  
-    addTodo(todoText) {
-      const todo = {
-        id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
-        text: todoText,
-        complete: false
-      };
-  
-      this.todos.push(todo);
-      this._commit(this.todos);
-    }
-  
-    // Map through all todos, and replace the text of the todo with the specified id
-    editTodo(id, updatedText) {
-      this.todos = this.todos.map(todo =>
-        todo.id === id
-          ? { id: todo.id, text: updatedText, complete: todo.complete }
-          : todo
-      );
-  
-      this._commit(this.todos);
-    }
-  
-    // Filter a todo out of the array by id
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-  
-      this._commit(this.todos);
-    }
-  
-    // Flip the complete boolean on the specified todo
-    toggleTodo(id) {
-      this.todos = this.todos.map(todo =>
-        todo.id === id
-          ? { id: todo.id, text: todo.text, complete: !todo.complete }
-          : todo
-      );
-  
-      this._commit(this.todos);
-    }
+  constructor() {
+    // The state of the model, an array of todo objects
+    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
   }
-  
-  export { Model };
+
+  bindTodoListChanged(callback) {
+    this.onTodoListChanged = callback;
+  }
+
+  _commit(todos) {
+    this.onTodoListChanged(todos);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false
+    };
+
+    this.todos.push(todo);
+    this._commit(this.todos);
+  }
+
+  // Map through all todos, and replace the text of the todo with the specified id
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map(todo =>
+      todo.id === id
+        ? { id: todo.id, text: updatedText, complete: todo.complete }
+        : todo
+    );
+
+    this._commit(this.todos);
+  }
+
+  // Filter a todo out of the array by id
+  deleteTodo(id) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+
+    this._commit(this.todos);
+  }
+
+  // Flip the complete boolean on the specified todo
+  toggleTodo(id) {
+    this.todos = this.todos.map(todo =>
+      todo.id === id
+        ? { id: todo.id, text: todo.text, complete: !todo.complete }
+        : todo
+    );
+
+    this._commit(this.todos);
+  }
+}
+
+export { Model };
